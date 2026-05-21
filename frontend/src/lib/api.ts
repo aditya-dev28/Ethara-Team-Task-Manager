75df9957c5fa92030ccec56799b935b46ff42550
Aditya Pratap Singh <aditya28178@gmail.com>
2026-05-22 02:17:26 +0530

    Updated production backend API URL

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "http://127.0.0.1:8001/api";
+export const API_BASE = "https://ethara-team-task-manager-production-f609.up.railway.app/api";
 
 export const api = axios.create({
   baseURL: API_BASE,
